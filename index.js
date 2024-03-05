95b6ee20e1fbf5d4349c674d7d60c442753ba8de
TayyabAhmed890 <160255633+TayyabAhmed890@users.noreply.github.com>
2024-03-05 19:55:00 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,67 @@
+//Overall We Learn..!
+// 1) Lets/Var/Const
+// 2) Variable/Datatypes
+// 3) camelCase/snake_case/PascalCase
+// 3) Array
+// 1) Lets/Var/Const
+//In both JavaScript and TypeScript, let, var, and const are used for variable declaration:
+//Let..!
+var myName = "Tayyab Ahmed";
+console.log(myName);
+//Var..!
+var someNum = 23;
+console.log(someNum);
+//const..!
+var fixValue = true;
+console.log(fixValue);
+// 2) Variable/Datatypes
+//Variable
+//In JavaScript and TypeScript, variables are containers for storing data values. These values can be of various types
+//Datatypes
+//In JavaScript, data types represent the kinds of values that can be used and manipulated in a program. JavaScript has several primitive data types and complex data types:
+//There are 7 Types of Datatypes We Learn..!
+// 1) String
+// 2) Number
+// 3) Boolean
+// 4) Undefined
+// 5) Null
+// 6) Symbol
+// 7) Bigint
+// String
+var str = "Welcome to Our Generative Ai Course..!";
+console.log(str);
+// Number
+var num = 20;
+console.log(num);
+// Boolean
+var bool = true;
+console.log(bool);
+// Undefined
+var noValue;
+console.log(noValue);
+// Null
+var nothing = null;
+console.log(nothing);
+// Symbol
+var objKey = Symbol("key");
+console.log(objKey);
+// Bigint
+var largeNumber = BigInt(123454653453423443254);
+console.log(largeNumber);
+// 3) camelCase/snake_case/PascalCase
+// In JavaScript This is the Standard of WorldWide Developers for assign a variable name in these following cases
+// 1) camelCase 
+// 1) snake_case 
+// 1) PascalCase 
+var myData = "Hello My Name is Tayyab Ahmed..!";
+console.log(myData);
+var my_number = 18;
+console.log(my_number);
+var MyResult = true;
+console.log(MyResult);
+// Array
+// In Javascript we Use Array for Storing Large Amount of Data 
+var carName = ["Toyota", "BMW", "Mercedes", "Honda"];
+var callOne = [carName[2]];
+console.log(carName);
+console.log(callOne);
---
+++ index.ts
@@ -0,0 +1,112 @@
+//Overall We Learn..!
+
+// 1) Lets/Var/Const
+// 2) Variable/Datatypes
+// 3) camelCase/snake_case/PascalCase
+// 3) Array
+
+
+// 1) Lets/Var/Const
+
+//In both JavaScript and TypeScript, let, var, and const are used for variable declaration:
+
+//Let..!
+
+let myName:string = "Tayyab Ahmed"
+console.log(myName);
+
+//Var..!
+
+let someNum:Number = 23;
+console.log(someNum);
+
+//const..!
+
+const fixValue:boolean = true;
+console.log(fixValue);
+
+// 2) Variable/Datatypes
+
+//Variable
+
+//In JavaScript and TypeScript, variables are containers for storing data values. These values can be of various types
+
+//Datatypes
+
+//In JavaScript, data types represent the kinds of values that can be used and manipulated in a program. JavaScript has several primitive data types and complex data types:
+
+//There are 7 Types of Datatypes We Learn..!
+
+// 1) String
+// 2) Number
+// 3) Boolean
+// 4) Undefined
+// 5) Null
+// 6) Symbol
+// 7) Bigint
+
+// String
+
+let str:string = "Welcome to Our Generative Ai Course..!";
+console.log(str);
+
+// Number
+
+let num:number = 20;
+console.log(num);
+
+// Boolean
+
+let bool:boolean = true;
+console.log(bool);
+
+// Undefined
+
+let noValue:undefined ;
+console.log(noValue);
+
+
+// Null
+
+let nothing:null = null;
+console.log(nothing);
+
+// Symbol
+
+let objKey:symbol = Symbol("key")
+console.log(objKey);
+
+// Bigint
+
+let largeNumber:bigint = BigInt(123454653453423443254);
+console.log(largeNumber);
+
+// 3) camelCase/snake_case/PascalCase
+
+// In JavaScript This is the Standard of WorldWide Developers for assign a variable name in these following cases
+
+// 1) camelCase 
+// 1) snake_case 
+// 1) PascalCase 
+
+let myData:String = "Hello My Name is Tayyab Ahmed..!"
+console.log(myData);
+let my_number:number = 18;
+console.log(my_number);
+let MyResult:boolean = true;
+console.log(MyResult);
+
+// Array
+
+// In Javascript we Use Array for Storing Large Amount of Data 
+
+let carName:string[] = ["Toyota","BMW","Mercedes","Honda"]
+let callOne = [carName[2]]
+console.log(carName);
+console.log(callOne);
+
+
+
+
+
+
